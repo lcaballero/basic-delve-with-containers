a5a343e827cf036371510781472d7f20cab64e77
lucas <lucas@section.io>
2020-01-27 11:25:41 -0700

    Adding deployment for k8s.

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+IMG="hw"
+TAG="wip"
+IT="$IMG:$TAG"
+CT="debug"
 
 # build makes the hw image
 build() {
-    docker build -t hw:wip .
+    docker build -t "$IT" .
 }
 
 # bash-in starts a bash session in the hw image
 bash-in() {
-    docker run -it --rm hw:wip bash
+    docker run -it --rm "$IT" bash
 }
 
 # delve starts a docker container running delve on the
@@ -17,17 +21,37 @@ bash-in() {
 delve() {
     docker run -it \
 	   -p 40000:40000 \
-	   --name debug \
-	   --rm hw:wip /go/bin/dlv \
+	   --name "$CT" \
+	   --rm "$IT" /go/bin/dlv \
 	   --listen=:40000 \
 	   --headless=true \
 	   --api-version=2 \
 	   exec /go/src/github.com/lcaballero/hello/hello-debug
 }
 
+apply() {
+    kubectl apply -f yamls/base-deployment.yaml
+}
+
+delete() {
+    kubectl delete -f yamls/base-deployment
+}
+
+logs() {
+    local name=$(kubectl get pods -A | \
+	grep 'debug-' | \
+	awk '{printf("%s\n", $2)}')
+    
+    kubectl logs $name
+}
+
+port-forward() {
+    kubectl port-forward deployment/debug 40000:40000
+}
+
+# debug via a delve connect
 debug() {
-    dlv connect 127.0.0.1:40000 \
-	--api-version=2	
+    dlv connect 127.0.0.1:40000 --api-version=2	
 }
 
 "$@"
